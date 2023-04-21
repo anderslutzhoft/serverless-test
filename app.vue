9dceb3f103f1b2a2b17ae2f38887195a77b5cddc
Anders Lützhøft <anders@lutzhoft.dk>
2023-04-21 14:06:32 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="testing">Test</button>
+  </div>
+</template>
+
+
+<script setup>
+
+const testing = () => {
+  const { data, error } = useFetch('./api/hello')
+  console.log("data",data.value)
+  console.log("error", error.value)
+}
+
+</script>
